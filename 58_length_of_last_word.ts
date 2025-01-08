2daa4b11d6522509cfe6d270e8e583f9ac9ef114
Nicolás Navarro <nicohnavarro@gmail.com>
2025-01-08 18:17:54 +0100

    Length of last word

---
+++ 58_length_of_last_word.ts
@@ -0,0 +1,21 @@
+function lengthOfLastWord(s: string): number {
+  const words = s.split(" ");
+  let res = /^[a-zA-Z]+$/;
+  let i = 1;
+  let ans = 0;
+  while (i <= words.length) {
+    let response = words[words.length - i];
+    if (res.test(response)) {
+      ans = response.length;
+      break;
+    }
+    i++;
+  }
+  return ans;
+}
+
+const test = ["Hello World", "hello world ", " Other word last  ", "a"];
+
+for (const word of test) {
+  console.log(lengthOfLastWord(word));
+}
