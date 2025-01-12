7bb5c361064a161e6862bc9d3b33dd2aec017b38
Nicolás Navarro <nicohnavarro@gmail.com>
2025-01-12 18:57:59 +0100

    14 Longest prefix

--- 14_longest_common_prefix.ts
+++ 14_longest_common_prefix.ts
@@ -13,20 +13,5 @@ function longestCommonPrefix(words: string[]): string {
   return prefix;
 }
 
-function longestCommonPrefixV2(words: string[]): string {
-  if (words.length === 0) return "";
-  if (words.length === 1) return words[0];
-  let prefix = "";
-
-  for (let i = 1; i < words.length; i++) {
-    while (words[i].indexOf(prefix) !== 0) {
-      prefix = prefix.slice(0, -1);
-      if (prefix === "") return "";
-    }
-  }
-
-  return prefix;
-}
-
 const testLongestCommonPrefix = ["doad", "door", "dock"];
 console.log(longestCommonPrefix(testLongestCommonPrefix));
