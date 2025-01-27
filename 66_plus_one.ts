df4e25f0baf56a685a4a0d8be5d076e36f134985
Nicolás Navarro <nicohnavarro@gmail.com>
2025-01-27 08:22:50 +0100

    Easy leetcode

---
+++ 66_plus_one.ts
@@ -0,0 +1,10 @@
+function plusOne(digits: number[]): number[] {
+  return (BigInt(digits.join("")) + BigInt(1))
+    .toString()
+    .split("")
+    .map((e) => Number(e));
+}
+
+const testPlusOne = [9, 9, 9];
+
+console.log(plusOne(testPlusOne));
